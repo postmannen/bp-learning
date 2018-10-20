23b5e1cf87db87c69a405d0c4238c9439617cfd2
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-10-20 07:17:37 +0200

    added off function for tracer

--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -85,5 +85,6 @@ func newRoom() *room {
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
 	}
 }
--- chapter1/trace/tracer.go
+++ chapter1/trace/tracer.go
@@ -11,16 +11,27 @@ type Tracer interface {
 	Trace(...interface{})
 }
 
+type tracer struct {
+	out io.Writer
+}
+
 //New Tracer
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
 }
 
-type tracer struct {
-	out io.Writer
-}
-
 func (t *tracer) Trace(a ...interface{}) {
 	fmt.Fprint(t.out, a...)
 	fmt.Fprintln(t.out)
+}
+
+// -----------
+
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+
+// Off creates a Tracer that will ignore calls to Trace.
+func Off() Tracer {
+	return &nilTracer{}
 }
