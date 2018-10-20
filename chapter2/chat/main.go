654a8ad2ae983cb8e8566b6c392c44b52e033968
Bjørn Tore Svinningen <postmannen@gmail.com>
2018-10-20 07:23:25 +0200

    created chapter2

---
+++ chapter2/chat/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+
+	"github.com/postmannen/bp-learning/chapter1/trace"
+)
+
+//templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//Load and execute the templates
+
+	t.once.Do(func() {
+		t.templ = template.Must(
+			//filepath.join will create the complete path ./templates/<filename>
+			template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the  application.")
+	flag.Parse() // parse the flags
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
+	//Root Handle.
+	//Here we send a type templateHandler directly in without
+	//creating a variabel/reference to it first. We can do that
+	//by preceding it with &.
+	//templateHandler have a serveHTTP method,
+	//and then becomes a handler
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	//start the room.
+
+	go r.run()
+
+	//Start the web server
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
